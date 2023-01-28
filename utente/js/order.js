337651b28d8ecfb4edcde3103bf9eea091c60fe6
AlbertoPavarin <albertopavarin@libero.it>
2023-01-28 08:09:37 +0100

    .

--- utente/js/order.js
+++ utente/js/order.js
@@ -1,20 +1,23 @@
 function setOrder(user_id, price, products)
 {
+    const breakValue = document.querySelector('#break-select').value;
+    const pickupValue = document.querySelector('#pickup-select').value;
+    
     let json = `{
         "user_ID":${user_id},
         "total_price":${price},
-        "break_ID": ${document.querySelector('#break-select').value},
+        "break_ID": ${breakValue},
         "status_ID": 1,
-        "pickup_ID": ${document.querySelector('#pickup-select').value},
+        "pickup_ID": ${pickupValue},
         "products": 
                 ${JSON.stringify(products)}
             ,
         "json": {
-        "user_ID": 1,
-        "total_price": 15.50,
-        "break_ID": 1,
+        "user_ID": ${user_id},
+        "total_price": ${price},
+        "break_ID": ${breakValue},
         "status_ID": 1,
-        "pickup_ID": 1,
+        "pickup_ID": ${pickupValue},
         "products": [
                 {"name": "panino al prosciutto", "price": 3, "quantity":1},
                 {"name": "panino al salame", "price": 3, "quantity":1},
