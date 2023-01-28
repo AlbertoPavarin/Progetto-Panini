10f9e4fc1d127a7f01a69bcfc5310f043428d42a
AlbertoPavarin <albertopavarin@libero.it>
2023-01-28 10:13:12 +0100

    ordini sembrano finiti

--- utente/order.php
+++ utente/order.php
@@ -68,7 +68,7 @@ function getJsonProductsForJson($cart)
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <script src="js/breakByPickup.js"></script>
-        <title>Categoria</title>
+        <title>Ordina</title>
         <link rel="stylesheet" href="static/css/style.css">
     </head>
     <body>
